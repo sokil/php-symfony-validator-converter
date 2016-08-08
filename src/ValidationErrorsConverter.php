545a1e15081f9b03784bb6ade27f449ffedf1816
Dmytro Sokil <dmytro.sokil@gmail.com>
2016-08-08 11:56:39 +0300

    init

---
+++ src/ValidationErrorsConverter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sokil\SiteCoreBundle\Converter;
+
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Form\FormInterface;
+
+class ValidationErrorsConverter
+{
+    public function formErrorsToArray(FormInterface $form)
+    {
+        $errors = array();
+
+        foreach ($form->getErrors() as $error) {
+            $errors[] = $error->getMessage();
+        }
+
+        /* @var $child \Symfony\Component\Form\Form */
+        foreach ($form->all() as $child) {
+            if (!$child->isValid()) {
+                $errors[$child->getName()] = $this->formErrorsToArray($child);
+            }
+        }
+
+        return $errors;
+    }
+
+    public function constraintViolationListToArray(ConstraintViolationListInterface $list)
+    {
+        /* @var ConstraintViolationInterface $violation */
+        $errors = [];
+
+        foreach($list as $violation) {
+            $errors[$violation->getPropertyPath()] = $violation->getMessage();
+        }
+
+        return $errors;
+    }
+
+}
